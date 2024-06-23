3f324b73b0139f5aa18d2e3167e798bfc34a9fad
Luiz Cavina <79873070+ZeroRaven23@users.noreply.github.com>
2024-06-23 03:05:38 -0300

    Fix URL for aws :)

--- docs/docs.go
+++ docs/docs.go
@@ -1451,7 +1451,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "3.145.165.61 :8080",
+	Host:             "3.145.165.61:8080",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Peru",
